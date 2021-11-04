129c3cb86be561ee04056f8f0678b23cdcf9a6a6
Choerun Asnawi <ochiwansa@users.noreply.github.com>
2021-11-04 08:53:13 +0700

    Update english.py

--- rules/english.py
+++ rules/english.py
@@ -27,7 +27,7 @@ class Rule:
   
   @classmethod
   def non_regular(cls, digit, head, tail):
-    unit_names = dict()
+    head_names = dict()
     head_separator = None
     tail_separator = None
     suffix = None
@@ -35,26 +35,26 @@ class Rule:
     if digit == 2:
       head_separator = ''
       tail_separator = '-'
-      unit_names[3] = 'thir'
-      unit_names[5] = 'fif'
-      unit_names[8] = 'eigh'
+      head_names[3] = 'thir'
+      head_names[5] = 'fif'
+      head_names[8] = 'eigh'
       if head == 1:
         suffix = 'teen' if tail > 2 else ''
         head = tail
         tail = 0
-        unit_names[0] = 'ten'
-        unit_names[1] = 'eleven'
-        unit_names[2] = 'twelve'
+        head_names[0] = 'ten'
+        head_names[1] = 'eleven'
+        head_names[2] = 'twelve'
       else:
-        unit_names[2] = 'twen'
-        unit_names[4] = 'for'
+        head_names[2] = 'twen'
+        head_names[4] = 'for'
     else:
       tail_separator = ' and ' if tail < 100 else ', '
     
     return {k: v for k, v in {
       'head': head,
       'tail': tail,
-      'unit_names': unit_names,
+      'head_names': head_names,
       'head_separator': head_separator,
       'tail_separator': tail_separator,
       'suffix': suffix,
